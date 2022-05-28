a9619bfca43b5dc6579dd4b3a144bde2d5923877
LakshayySharma <44471078+LakshayySharma@users.noreply.github.com>
2022-05-28 20:28:26 +0530

    student model created

---
+++ model/Student.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const studentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  grade: {
+    type: Number,
+    required: true,
+  },
+  favouriteSubject: {
+    type: String,
+  },
+});
+
+module.exports = Student = mongoose.model("student", studentSchema);
